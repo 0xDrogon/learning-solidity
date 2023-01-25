cdbc1255f079ef6dbe5cf8cdf579eec75afb2728
0xDrogon <tiagofsdomingues@gmail.com>
2023-01-25 13:58:08 +0000

    updates FundMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -4,51 +4,67 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
+error FundMe__NeedMoreETH();
+error FundMe__FailedWithdraw();
 
 contract FundMe {
     using PriceConverter for uint256;
 
+    mapping(address => uint256) private s_addressToAmountFunded;
+    address[] private s_funders;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    address public immutable i_owner;
-    address[] public funders;
-    mapping(address => uint256) public addressToAmountFunded;
-    AggregatorV3Interface public priceFeed;
-
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
+    AggregatorV3Interface private s_priceFeed;
 
     modifier onlyOwner {
-        if (msg.sender != i_owner) revert NotOwner(); //require(msg.sender == i_owner, "Sender is not owner!");
+        if (msg.sender != i_owner) revert FundMe__NotOwner(); //require(msg.sender == i_owner, "Sender is not owner!");
         _;
     }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to send more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-    
-    function withdraw() public onlyOwner {
-        for (uint256 i = 0; i < funders.length; i++){
-            address funder = funders[i];
-            addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-
-    fallback() external payable {
-        fund();
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     receive() external payable {
         fund();
     }
 
+    fallback() external payable {
+        fund();
+    }
+
+    function fund() public payable {
+        if (msg.value.getConversionRate(s_priceFeed) < MINIMUM_USD) revert FundMe__NeedMoreETH(); //require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to send more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    function withdraw() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        for (uint256 i = 0; i < funders.length; i++){
+            address funder = funders[i];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        if (!callSuccess) revert FundMe__FailedWithdraw(); //require(callSuccess, "Call failed");
+    }
+
+    function getOwner() public view returns(address) {
+        return i_owner;
+    }
+
+    function getFunder(uint256 index) public view returns(address) {
+        return s_funders[index];
+    }
+
+    function getAddressToAmountFunded(address funder) public view returns(uint256) {
+        return s_addressToAmountFunded[funder];
+    }
+
+    function getPriceFeed() public view returns(AggregatorV3Interface) {
+        return s_priceFeed;
+    }
 }
-
-
